4939ceb9a75c651e4a019a0aa310d2d02f94aeb2
TrickStealer <pritykinigor@gmail.com>
2024-09-24 14:52:21 +0200

    Update code for Mocha

--- tests/select_the_program.js
+++ tests/select_the_program.js
@@ -2,34 +2,34 @@ import { Builder, By, Key } from 'selenium-webdriver';
 import { should } from 'chai';
 should();
 
-async function selectTheProgram(class_name) {
-  let driver = await new Builder().forBrowser('chrome').build();
+describe("Select the program", function(){
+  it("violetLight", async function(){
+    const class_name = 'program-type__el--violetLight'
 
-  try {
-    await driver.get("https://letbefit.ru/");
+    let driver = await new Builder().forBrowser('chrome').build();
 
-    let element = await driver.findElement(By.className(class_name));
-    // element.click();
+    try {
+      await driver.get("https://letbefit.ru/");
 
-    await element.getAttribute("class").then((text) => {
-      text.split(' ').should.include(
-        'active',
-        `Test "selectTheProgram": element "${class_name}" is not active`
-      );
-      console.log(`Test "selectTheProgram": element "${class_name}" is active`);
-    })
-  }
-  catch(message) {
-    console.log(message);
-  }
-  finally {
-    await driver.quit();
-  }
-}
+      let element = await driver.findElement(By.className(class_name));
+      element.click();
 
-selectTheProgram('program-type__el--seaLight')
-selectTheProgram('program-type__el--blueLight')
-selectTheProgram('program-type__el--pinkLight')
-selectTheProgram('program-type__el--swampLight')
-selectTheProgram('program-type__el--orangeLight')
-selectTheProgram('program-type__el--violetLight')
+      await element.getAttribute("class").then((text) => {
+        text.split(' ').should.include(
+          'active',
+          `Element "${class_name}" is not active`
+        );
+      })
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+});
+
+// selectTheProgram('program-type__el--seaLight')
+// selectTheProgram('program-type__el--blueLight')
+// selectTheProgram('program-type__el--pinkLight')
+// selectTheProgram('program-type__el--swampLight')
+// selectTheProgram('program-type__el--orangeLight')
+// selectTheProgram('program-type__el--violetLight')
