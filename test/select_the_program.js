52754fd618cfcf1994f01a70271cfbe9a105095c
TrickStealer <pritykinigor@gmail.com>
2024-09-24 17:55:28 +0200

    Change name of folder "tests" - > "test" for more simple calling of mocha

--- test/select_the_program.js
+++ test/select_the_program.js
@@ -2,7 +2,35 @@ import { Builder, By, Key } from 'selenium-webdriver';
 import { should } from 'chai';
 should();
 
+
+
+
+
+
 describe("Select the program", function(){
+  it("seaLight", async function(){
+    const class_name = 'program-type__el--seaLigt'
+
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+      await driver.get("https://letbefit.ru/");
+
+      let element = await driver.findElement(By.className(class_name));
+      element.click();
+
+      await element.getAttribute("class").then((text) => {
+        text.split(' ').should.include(
+          'active',
+          `Element "${class_name}" is not active`
+        );
+      })
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
   it("violetLight", async function(){
     const class_name = 'program-type__el--violetLight'
 
@@ -12,7 +40,7 @@ describe("Select the program", function(){
       await driver.get("https://letbefit.ru/");
 
       let element = await driver.findElement(By.className(class_name));
-      element.click();
+      // element.click();
 
       await element.getAttribute("class").then((text) => {
         text.split(' ').should.include(
