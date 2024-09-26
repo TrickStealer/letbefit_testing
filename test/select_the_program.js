68d471d2564d290396fcd84648c09202c400333d
TrickStealer <pritykinigor@gmail.com>
2024-09-27 00:49:35 +0200

    Add all tests for selecting the program. One is asserting. Needs to fix bug and refactoring

--- test/select_the_program.js
+++ test/select_the_program.js
@@ -3,6 +3,7 @@ import { Builder, By, Key } from 'selenium-webdriver';
 import { should } from 'chai';
 should();
 
+
 async function algorithm(driver, program_class, diet_name, diet_selector) {
   await driver.get(config.web_site);
 
@@ -26,6 +27,7 @@ async function algorithm(driver, program_class, diet_name, diet_selector) {
     );
   })
 }
+
 
 describe("Select the program", function(){
   it("Снижение веса - Extralight", async function(){
@@ -88,11 +90,161 @@ describe("Select the program", function(){
       await driver.quit();
     }
   });
+
+  it("Поддержание формы - Balance Premium", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--blueLight';
+    const diet_name = "balancepremium";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+
+  it("Набор массы - Strong", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--pinkLight';
+    const diet_name = "strong";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+  it("Набор массы - Strong Premium", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--pinkLight';
+    const diet_name = "strongpremium";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+  it("Набор массы - Super Strong", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--pinkLight';
+    const diet_name = "superstrong";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+
+  it("Питание без мяса - Vegan", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--swampLight';
+    const diet_name = "veggi";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+  it("Питание без мяса - Fish", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--swampLight';
+    const diet_name = "fish";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+  it("Питание без мяса - Средизем­номорская диета", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--swampLight';
+    const diet_name = "middlesea";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+
+  it("Очищение организма - Gluten Lacto Free", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--orangeLight';
+    const diet_name = "gluten_free";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+
+  it("Питание в офис - Everydaily", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--violetLight';
+    const diet_name = "everydaily";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
+
+  it("Питание в офис - Daily", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const program_class = 'program-type__el--violetLight';
+    const diet_name = "daily";
+    const diet_selector = `.program-group__el [data-program="${diet_name}"]`;
+
+    try {
+      await algorithm(driver, program_class, diet_name, diet_selector);
+    }
+    finally {
+      await driver.quit();
+    }
+  });
 });
 
-// selectTheProgram('program-type__el--seaLight')
-// selectTheProgram('program-type__el--blueLight')
-// selectTheProgram('program-type__el--pinkLight')
-// selectTheProgram('program-type__el--swampLight')
 // selectTheProgram('program-type__el--orangeLight')
 // selectTheProgram('program-type__el--violetLight')
