c89431c1350b0f925a9413016d5ec6b170660130
TrickStealer <pritykinigor@gmail.com>
2024-09-26 14:43:48 +0200

    Add new test of scrolling the page by button

---
+++ test/scrolling.js
@@ -0,0 +1,43 @@
+import { Builder, By, Key } from 'selenium-webdriver';
+import { should } from 'chai';
+should();
+
+describe("Scrolling to form", function(){
+  it("By ckick the button 'Выбрать программу'", async function(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    const css_button = '.slider-main .slick-current .md__hide .action-select-topbar-program';
+    const css_text = '.program-main-wrap .type--w700';
+
+    const scroll_check_period = 500; // ms
+    const scroll_check_limit = 10000; // ms
+
+    try {
+      await driver.get("https://letbefit.ru/");
+      await driver.findElement(By.css(css_button)).click();
+
+      const text_Y = await driver.findElement(By.css(css_text)).getRect().then((value) => {return value.y;});
+
+      let window_H_Y = await driver.executeScript(`return [window.innerHeight, window.scrollY];`);
+
+      let is_text_visible = (window_H_Y[1] < text_Y) && (text_Y < window_H_Y[1] + window_H_Y[0]);
+
+      let n = 0;
+
+      while (!is_text_visible) {
+        await driver.sleep(scroll_check_period);
+
+        window_H_Y = await driver.executeScript(`return [window.innerHeight, window.scrollY];`);
+
+        is_text_visible = (window_H_Y[1] < text_Y) && (text_Y < window_H_Y[1] + window_H_Y[0]);
+
+        n.should.to.be.below(scroll_check_limit / scroll_check_period,
+          `Page not scrolled more than ${scroll_check_limit / 1000} seconds`);
+      }
+    }
+    finally {
+      // await driver.sleep(5000);
+      await driver.quit();
+    }
+  });
+});
